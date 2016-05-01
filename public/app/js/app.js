003d0627b1aeb0c7e597589f56a229b07237b1d3
Timidfriendly <timid@point-online.org>
2016-05-01 17:58:35 +0200

    ADD: review-form toggle controller

--- public/app/js/app.js
+++ public/app/js/app.js
@@ -18,7 +18,11 @@
             return {
                 restrict: 'E',
                 templateUrl: 'partials/review-form.html',
-                replace: true
+                replace: true,
+                controller: function () {
+                    this.showForm = false;
+                },
+                controllerAs: 'reviewFormToggleCtrl'
             }
         })
     ;
